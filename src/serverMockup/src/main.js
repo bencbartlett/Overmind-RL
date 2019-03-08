206965e2381debcd3280d98f98ab5371e3e75123
Ben Bartlett <benjamincbartlett@gmail.com>
2019-03-08 14:44:35 -0800

    initial commit

---
+++ src/serverMockup/src/main.js
@@ -0,0 +1,5 @@
+const ScreepsServer = require('./screepsServer');
+const stdHooks = require('../utils/stdhooks');
+const TerrainMatrix = require('./terrainMatrix');
+
+module.exports = { ScreepsServer, stdHooks, TerrainMatrix };
