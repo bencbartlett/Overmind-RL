1ee82e45fe4e011fa5b92c462896bb4fe804f248
Ben Bartlett <benjamincbartlett@gmail.com>
2019-06-08 12:18:00 -0700

    define configuration for PPO, QMIX, APEX-QMIX

--- models/train_multiagent_vectorized.py
+++ models/train_multiagent_vectorized.py
@@ -3,62 +3,22 @@ import argparse
 import gym
 import ray
 from ray import tune
-from ray.rllib.agents.qmix import QMixTrainer
 
 from screeps_rl_env import ScreepsMultiAgentVectorEnv, CreepAgent
 
-# def training_workflow(config, reporter):
-#
-#     # Setup policy and policy evaluation actors
-#
-#     with tf.Session() as sess:
-#         workers = [
-#             PolicyEvaluator.as_remote().remote(
-#                     lambda config: ScreepsMultiAgentEnv(config, worker_index = i, num_envs = 10),
-#                     # lambda config: ScreepsEnv(config, worker_index = i, vector_index = 0),
-#                     # lambda c: gym.make("CartPole-v0"),
-#                     PPOTFPolicy)
-#             for i in range(config["num_workers"])
-#         ]
-#
-#         # TODO: hardcoded spaces
-#         observation_space = gym.spaces.MultiDiscrete([50, 50, 50, 50])
-#         action_space = gym.spaces.Discrete(8)
-#
-#         policy = PPOTFPolicy(observation_space, action_space, {})
-#
-#         for _ in range(config["num_iters"]):
-#             # Broadcast weights to the policy evaluation workers
-#             weights = ray.put({"default_policy": policy.get_weights()})
-#             for w in workers:
-#                 w.set_weights.remote(weights)
-#
-#             # Gather a batch of samples
-#             batch = SampleBatch.concat_samples(ray.get([w.sample.remote() for w in workers]))
-#
-#             # Improve the policy using the batch
-#             policy.learn_on_batch(batch)
-#
-#             reporter(**collect_metrics(remote_evaluators = workers))
-
+parser = argparse.ArgumentParser(description="Train multi-agent model")
+parser.add_argument("--model", type=str, default="APEX_QMIX")
+parser.add_argument("--cluster", type=bool, default=False)
+parser.add_argument("--num_workers", type=int, default=6)
+parser.add_argument("--num_envs_per_worker", type=int, default=10)
+parser.add_argument("--grouped", type=bool, default=True)
 
 if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description="Train multi-agent model")
-    parser.add_argument("--cluster", type=bool, default=False)
-    parser.add_argument("--num_workers", type=int, default=6)
-    parser.add_argument("--num_envs_per_worker", type=int, default=10)
-    parser.add_argument("--grouped", type=bool, default=True)
-
     args = parser.parse_args()
 
     if args.cluster:
-        # Running on a cluster
         ray.init(redis_address="localhost:6379")
-        print("---Running on cluster---")
     else:
-        # Running on local machine
-        print("---Running locally---")
         ray.init()
     print("Cluster resources:", ray.cluster_resources())
 
@@ -79,53 +39,68 @@ if __name__ == "__main__":
     observation_space_grouped = gym.spaces.Tuple([observation_space] * len(grouping["Agent1"]))
     action_space_grouped = gym.spaces.Tuple([action_space] * len(grouping["Agent1"]))
 
+    # Register environments
     tune.register_env("screeps_multiagent_vectorized",
+                      lambda config: ScreepsMultiAgentVectorEnv(config, num_envs=args.num_envs_per_worker))
+
+    tune.register_env("screeps_multiagent_vectorized_grouped",
                       lambda config:
                       ScreepsMultiAgentVectorEnv(config, num_envs=args.num_envs_per_worker).with_agent_groups(
                           grouping, obs_space=observation_space_grouped, act_space=action_space_grouped
                       ))
 
-    # policies = {
-    #     creep.agent_id: (None, observation_space, action_space, {}) for creep in agents
-    # }
+    config = {
+        # "lr"         : grid_search([1e-2 , 1e-4, 1e-6]),  # try different lrs
+        "num_gpus": 0,
+        "num_workers": args.num_workers,  # parallelism
+        "num_envs_per_worker": args.num_envs_per_worker,
+        "env_config": {
+            "agents": agents,
+            "use_backend": False,
+        },
+    }
 
-    # trainer = QMixTrainer(
-    #     env="screeps_multiagent_vectorized",
-    #     config={
-    #         "num_workers": 0,  # args.num_workers,  # parallelism
-    #         "num_envs_per_worker": args.num_envs_per_worker,
-    #         # "remote_worker_envs": True,
-    #         "env_config": {
-    #             "agents": agents,
-    #             "use_backend": False,
-    #         },
-    #     }
-    # )
-    #
-    # trainer.train()
+    if args.model == "QMIX":
+        config = {
+            **config,
+            "env": "screeps_multiagent_vectorized_grouped",
+            "learning_starts": 5000,
+            "double_q": False,
+        }
+        group = True
+    elif args.model == "APEX_QMIX":
+        config = {
+            **config,
+            "env": "screeps_multiagent_vectorized_grouped",
+            "buffer_size": 200000,
+            "learning_starts": 5000,
+            "sample_batch_size": 32,
+            "timesteps_per_iteration": 2500,
+            "double_q": False,
+        }
+        group = True
+    elif args.model == "PG":
+        policies = {
+            creep.agent_id: (None, observation_space, action_space, {}) for creep in agents
+        }
+        config = {
+            **config,
+            "env": "screeps_multiagent_vectorized",
+            "multiagent": {
+                "policies": policies,
+                "policy_mapping_fn": tune.function(lambda agent_id: agent_id),
+            }
+        }
+        group = False
 
     tune.run(
-        "QMIX",
+        args.model,
         stop={
-            "timesteps_total": 5e5,
+            "timesteps_total": 1e9,
         },
-        config={
-            "env": "screeps_multiagent_vectorized",
-            # "lr"         : grid_search([1e-2 , 1e-4, 1e-6]),  # try different lrs
-            "num_gpus": 0,
-            "num_workers": 0,#args.num_workers,  # parallelism
-            "num_envs_per_worker": args.num_envs_per_worker,
-            # "remote_worker_envs": True,
-            "env_config": {
-                "agents": agents,
-                "use_backend": False,
-            },
-            # "multiagent": {
-            #     "policies": policies,
-            #     "policy_mapping_fn": tune.function(lambda agent_id: agent_id),
-            # }
-        },
+        config=config,
         checkpoint_freq=100,
         checkpoint_at_end=True,
-        queue_trials=True
+        reuse_actors=True,
+        queue_trials=False
     )
